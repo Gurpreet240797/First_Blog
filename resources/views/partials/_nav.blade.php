2ff9cb972321431a6a38fc0f07bcb7615779c0a4
Gurpreet Singh <gurpreet24277@gmail.com>
2017-07-12 20:36:25 +0530

    Authentication set

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -1,5 +1,4 @@
 
-
 <nav class="navbar navbar-default">
             <div class="container">
 
@@ -10,7 +9,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                    <a href="#" class="navbar-brand">Gurpreet Singh</a>
+                    <a href="/" class="navbar-brand">Gurpreet Singh</a>
                 </div>
 
                 <!--MENU-->
@@ -24,16 +23,23 @@
 
                     <!--right side-->
                     <div class="nav navbar-nav navbar-right">
+                    @if(Auth::check())
                          <li class="dropdown ">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">My Account <span class="caret"></span></a>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Hello {{ Auth::user()->name }} <span class="caret"></span></a>
                             <ul class="dropdown-menu">
-                                <li class="{{ Request::is('posts')? "active" : "" }}"><a href="/posts">Posts</a></li>
+                                <li class="{{ route('login') }}"><a href="/posts">Posts</a></li>
                                 <li class="divider"></li>
-                                <li><a href="#">Log Out</a></li>
+                                <li><a href="{{ route('logout') }}">Log Out</a></li>
                             </ul>
                         </li>
+
+                    @else
+                         <a class="btn btn-default" href="{{ route('login') }}" role="button">Log In</a>
+                         <a class="btn btn-default" href="{{ route('register') }}" role="button">Register</a>
+
+                    @endif
                     </div>
                     
                 </div>
             </div>
-        </nav>
+</nav>
